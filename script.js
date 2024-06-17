a791036f1ce2094ce665d604947fd29a5cfe216e
rajeevjillella <96594281+rajeevjillella@users.noreply.github.com>
2024-06-17 12:58:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function shortcut(s1, s2) {
   // your code here
+	if (s1.length === 0 || s2.length === 0){
+
+        return '';
+    }
+
+    const firstchas1 = s1.charAt[0];
+    const firstchas2 = s2.charAt[0];
+
+    return firstchas1 + firstchas2;
 }
 
 // Do not change the code below.
